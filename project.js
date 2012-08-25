4c2682eb10264fc6751e1ee6c19dc1ace2181613
Mohammad Abbas <mohammad.abbas86@gmail.com>
2012-08-26 00:44:20 +0100

    circles are now moving

--- project.js
+++ project.js
@@ -1,8 +1,47 @@
-(function(){
-            var width = 500;
-            var height = 200;
-        	var svg = d3.select("#tank").append("svg").attr("width", width).attr("height", height);
-            var circle = svg.selectAll("circle");
-        })();
-        
-        
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+function Fish (width, height) {
+    this.positionX= Math.floor(Math.random() * width);
+    this.positionY = Math.floor(Math.random() * height);
+    this.size = 15;
+}
+
+Fish.prototype.getPositionX = function() {
+    return this.positionX;
+};
+Fish.prototype.setPositionX = function(positionX) {
+    this.positionX = positionX;
+};
+Fish.prototype.getPositionY = function() {
+    return this.positionY;
+};
+Fish.prototype.getSize = function() {
+    return this.size;
+};
+
+var fish = new Array(20);
+for (var i = 0; i < fish.length; i++) {
+	fish[i] = new Fish(width, height);
+}
+
+var svg = d3.select("#tank")
+			.append("svg").attr("width", width)
+			.attr("height", height);
+
+
+var circles = svg.selectAll("circle")
+				 .data(fish)
+				 .enter()
+				 .append("circle")
+				 .attr("fill", "steelblue");
+
+circles.attr("cx", function(d, i) {return d.getPositionX();})
+	   .attr("cy", function(d, i) {return d.getPositionY();})
+	   .attr("r", function(d, i) {return d.getSize();})
+  
+d3.timer(function() {
+	circles.transition()
+		   .attr("cx", function (d,i) {d.setPositionX(d.getPositionX() + 10); return d.getPositionX();})
+		   .delay(500);
+});
